10e8f94b9794113638acc5fcd7db1179d2c61157
leondealex <deleonavalosa@gmail.com>
2023-03-13 16:38:20 -0600

    COMENTARIOS EN EL CODIGO

--- app.js
+++ app.js
@@ -10,6 +10,21 @@ function bigWords(string, wordsArray) {
   return result;
 }
 
+/*
+function bigWords(string, wordsArray) { ... } - Esta es una
+función que toma dos parámetros: una cadena string y un array
+wordsArray. La función itera a través del array y compara la
+longitud de cada palabra en el array con la longitud de la cadena.
+Si la longitud de la palabra en el array es mayor que la longitud
+de la cadena, se agrega la palabra al array result. Finalmente,
+la función devuelve el array result que contiene todas las
+palabras en el array wordsArray que son más largas que la
+cadena string.
+*/
+
+
+
+
 // Paso 2: Función printArray
 function printArray(wordsArray) {
   const list = document.querySelector("#words-list");
@@ -20,6 +35,17 @@ function printArray(wordsArray) {
     list.appendChild(li);
   }
 }
+
+/*
+function printArray(wordsArray) { ... } - Esta función toma un
+array wordsArray y crea elementos HTML <li> para cada elemento
+en el array. Luego, estos elementos se agregan a un elemento HTML
+<ul> con el id words-list. El contenido anterior del elemento
+<ul> se borra antes de agregar los nuevos elementos.
+*/
+
+
+
 
 // Paso 3: Array de palabras
 const words = [
@@ -35,6 +61,15 @@ const words = [
   "cocodrilo",
 ];
 
+/*
+const words = [...] - Esta es una matriz de palabras utilizadas
+por la función bigWords para buscar palabras más largas que la
+cadena de búsqueda.
+*/
+
+
+
+
 // Paso 5: Función para manejar el clic del botón
 function handleButtonClick() {
   const input = document.querySelector("#search-input");
@@ -43,6 +78,25 @@ function handleButtonClick() {
   printArray(result);
 }
 
+/*
+function handleButtonClick() { ... } - Esta función se ejecuta
+cuando se hace clic en el botón con el id search-button. Recupera
+el valor del campo de entrada con el id search-input, llama a la
+función bigWords con ese valor y la matriz words como argumentos
+y luego llama a la función printArray con el resultado.
+*/
+
+
+
+
+
 // Event listener para el botón
 const button = document.querySelector("#search-button");
 button.addEventListener("click", handleButtonClick);
+/*
+const button = document.querySelector("#search-button");
+- Esto recupera el botón HTML con el id search-button.
+button.addEventListener("click", handleButtonClick);
+- Esto agrega un evento click al botón search-button que
+ejecutará la función handleButtonClick cuando se haga clic en él.
+*/
